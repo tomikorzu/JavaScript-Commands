d8388dee5611f1dedd387892af5bc4aef0387f84
tomikorzu <tomykorzu@icloud.com>
2024-07-08 11:45:40 -0300

    feat(ejercicios-consola): add exercises of console in javascript

---
+++ gm2/ejercicios-consola/consola.js
@@ -0,0 +1,30 @@
+//Ejercicio 1
+console.log('hola mundo');
+let nombre = 'tomas korzusehec';
+let ciudadPais = 'Barracas, Argentina'
+console.log('Mi nombre es', nombre, 'y vivo en',ciudadPais);
+
+//ejercicio 2
+console.log('Bienvenido al sistema');
+console.info('Cargando Aplicacion...');
+console.warn('Advertencia: Espacio en disco bajo');
+console.error('Error: No se pudo conectar al servidor')
+
+//ejercicio 3
+console.log('  *')
+console.log(' * *')
+console.log('* * *')
+
+//Ejercicio 4
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+debugger;
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10)
+
---
+++ gm2/gm2.js
@@ -0,0 +1,22 @@
+//console.clear()//limpia consola
+//console.() sirve solo para veificar la funcionalidad de codigo, solo lo usa el desarrollador
+console.log('hola')
+console.error('ocurrio un error')//rojo
+console.info('informacion')//normal
+console.warn('advertencia')//amarillo
+console.debug('debug')
+//ejemplo
+const nombre = 'tomas';
+let edad = 18;
+edad = 17;
+console.log('mi nombre es',nombre, 'y tengo', edad);
+if(edad >= 18) {
+    console.log('es mayor de edad');
+}
+else{
+    console.error('es menor de edad');
+}
+
+
+
+
