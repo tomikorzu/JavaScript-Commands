b837ddd6cc8ca881e0a6f774b6017604ba4f282f
tomikorzu <tomykorzu@icloud.com>
2024-08-16 12:25:41 -0300

    feat(forEach, For of and For In excercises)

--- for/main.js
+++ for/main.js
@@ -378,3 +378,397 @@ function mostrarPromedioEstrellas(calificaciones){
 }
 
 console.log(mostrarPromedioEstrellas([4, 6, null, 2, 4]))
+
+// forEach
+console.log('Ejemplo forEach')
+let users = [{name: 'tomas', age: 18}, {name: 'Juan', age: 20}, {name: 'dibu', age: 30}]
+users.forEach(function(user, index, arr){
+    console.log(index)
+})
+
+console.log('Ejemplo for Of')
+let edades =[20, 19 ,14, 15]
+for (let listaEdades of edades){
+    console.log(listaEdades)
+}
+console.log('Ejemplo for In')
+let users2 = {
+    name:'tomas',
+    edad: 18
+}
+for (let info in users2){
+    console.log(info)
+}
+
+// Ejercicio 1
+// Imagina que sos parte del equipo de desarrollo de Boca, y tu tarea es gestionar el
+// presupuesto del club de fútbol. Cada categoría de gasto del club, como sueldos de
+// jugadores, mantenimiento del estadio, y costos de viajes, se representa como una
+// propiedad en un objeto. Tu tarea es crear una función que calcule el total de todos los
+// gastos del club utilizando un bucle for...in.
+// La función debe:
+// a. Recibir un objeto gastos donde cada propiedad representa una categoría de
+// gasto y su valor es el monto de gasto en esa categoría.
+// b. Utilizar un bucle for...in para recorrer las propiedades del objeto gastos y
+// sumar los valores de cada propiedad.
+// c. Mostrar el total de los gastos en la consola
+console.log('Ejercicio 1')
+//For in
+let gastosTotales = 0
+let gastos = {
+    sueldosJugadores: 2000,
+    mantenimientoEstadio:1500,
+    costosViaje: 500
+}
+function calcularGastosTotales(gastos){
+    for (let propiedadesGastos in gastos){
+        gastosTotales += gastos[propiedadesGastos] 
+    }
+    return `Los gastos totales son: ${gastosTotales}`
+}
+console.log(calcularGastosTotales(gastos))
+
+// Ejercicio 2
+// Después de haber calculado el total de gastos , ahora necesitas saber cómo se
+// distribuyen esos gastos en diferentes categorías. Tienes un objeto que representa las
+// categorías de gastos del club, con cada propiedad indicando el tipo de gasto y su
+// monto.
+// Tu tarea es crear una función que:
+// a. Reciba un objeto gastos donde cada propiedad representa una categoría de
+// gasto y su valor es el monto gastado en esa categoría.
+// b. Utilice un bucle for...in para recorrer las categorías de gasto.
+// c. Imprima el monto gastado en cada categoría y calcule el total general de los
+// gastos.
+// d. Muestre en la consola cuánto se gastó en cada categoría y el total general, para
+// entender mejor cómo se distribuyen los gastos.
+console.log('Ejercicio 2')
+function categoriasGastos (gastos){
+    let propiedadYGasto = 'Las propiedades con sus gastos son: '
+    for (let propiedadesCategorias in gastos){
+        propiedadYGasto += propiedadesCategorias + ': ' + gastos[propiedadesCategorias] + ' '
+    }
+    console.log(propiedadYGasto)
+}
+console.log(categoriasGastos(gastos))
+console.log(calcularGastosTotales(gastos / 2))
+
+// Ejercicio 3
+// Imagina que trabajás en el equipo de desarrollo de FIFA y necesitás actualizar las
+// estadísticas de los jugadores cada año después de la temporada. Tu tarea es crear una
+// función que tome un objeto con las estadísticas actuales del jugador y un objeto con los
+// nuevos valores a añadir. La función debe usar un bucle for...in para recorrer las
+// propiedades del objeto de estadísticas y actualizar los valores correspondientes.
+// La función debe:
+// a. Recibir un objeto jugador con las propiedades nombre, asistencias, y
+// goles, que contienen las estadísticas actuales del jugador.
+// b. Recibir un objeto nuevasEstadisticas con los valores de asistencias y
+// goles que deben añadirse.
+// c. Usar un bucle for...in para recorrer las propiedades de
+// nuevasEstadisticas y añadir los valores a las estadísticas del jugador.
+// d. Verificar que los nuevos valores sean números válidos (mayores o iguales a
+// cero) antes de realizar la actualización.
+// e. Devolver un objeto con las estadísticas actualizadas para el jugador
+console.log('Ejercicio 3')
+let jugador = {
+    nombre: 'Leo Messi',
+    asistencias: 32,
+    goles: 67
+}
+let nuevosValores = {
+    asistencias: 37,
+    goles: 53
+}
+// For In
+function actualizarEstadisticas(jugador, nuevosValores){
+    console.log(`Estadisticas pasadas`)
+    console.log(jugador)
+    for (let prodpiedadesJugador in nuevosValores){
+        if (nuevosValores[prodpiedadesJugador] < 0){
+            console.log(`ERROR: ${prodpiedadesJugador} invalidos/as`)
+            break
+        } else{
+            jugador.asistencias = nuevosValores.asistencias
+            jugador.goles = nuevosValores.goles
+        }
+    }
+    console.log(`Estadisticas actualizadas`)
+    console.log(jugador)
+}
+console.log(actualizarEstadisticas(jugador, nuevosValores))
+
+// Ejercicio 4
+// Imaginá que trabajás en el equipo de análisis de datos del Mundial de la FIFA.
+// Necesitás saber cuántas veces aparece una letra específica en el nombre de un
+// jugador.
+// Tu tarea es escribir un programa que:
+// a. Reciba una cadena de texto cadena que contiene el nombre de un jugador y un
+// carácter caracter que deseas contar.
+// b. Utilice un bucle for...of para recorrer cada carácter en la cadena de texto.
+// c. Cuente cuántas veces aparece el carácter dado en el nombre del jugador.
+// d. Muestra el resultado indicando cuántas veces aparece el carácter en el nombre
+
+console.log('Ejercicio 4')
+function verificarLetraEnNombre(nombre, letra){
+    let letraRepetida = 0
+    for (let propiedadesNombre of nombre){
+        if (propiedadesNombre.includes(letra)){
+            letraRepetida++
+        }
+    }
+    console.log(`La letra ${letra}, en el nombre ${nombre}, se repite ${letraRepetida} veces`)
+}
+console.log(verificarLetraEnNombre('matematicas', 'a'))
+
+
+// Ejercicio 5
+// Imaginá que estás trabajando en el sistema de compras del Mundial de la FIFA y
+// necesitas calcular el costo total de todos los productos en el carrito de compras de un
+// fanático. Tienes un array que contiene objetos, donde cada objeto representa un
+// producto con propiedades como nombre y precio.
+// Tu tarea es crear un programa que:
+// a. Reciba un array de objetos productos, donde cada objeto tiene propiedades
+// nombre y precio.
+// b. Utilice un bucle for...of para recorrer cada objeto en el array.
+// c. Sume los precios de todos los productos en el carrito.
+// d. Muestra el costo total acumulado, que será el monto que debe pagar el cliente
+// por su compra.
+console.log('Ejercicio 5')
+function calcularCostosTotales(carritoCompras){
+    let precios = 0
+    for (let propiedadesCarritoCompras of carritoCompras){
+        console.log(propiedadesCarritoCompras)
+        precios += propiedadesCarritoCompras.precio
+    }
+    console.log(`El cliente debera pagar ${precios}`)
+}
+console.log(calcularCostosTotales(
+    [
+        {nombre: 'Camiseta', precio: 100}, {nombre: 'Botines', precio: 140}, {nombre: 'short', precio: 60}
+    ]
+))
+
+// Ejercicio 6
+// estás trabajando en el sistema de ventas de tickets para la Copa América. Tienes un
+// array de objetos donde cada objeto representa un ticket disponible, con propiedades
+// nombre (del evento) y disponible (un booleano que indica si el ticket está disponible
+// o agotado).
+// Tu tarea es escribir un programa que:
+// a. Reciba un array de objetos tickets, donde cada objeto tiene propiedades
+// nombre y disponible.
+// b. Utilice un bucle for...of para recorrer cada objeto en el array.
+// c. Genere un informe que muestre, para cada ticket, si está disponible o agotado.
+// d. Presenta el nombre del evento seguido de su estado de disponibilidad.
+console.log('Ejercicio 6')
+function verificarTicketDisponible (ticketDisponible){
+    for (propiedadesTicket of ticketDisponible){
+        if (propiedadesTicket.disponible){
+            console.log(propiedadesTicket.nombre + ' esta disponible')
+        }
+        if (!propiedadesTicket.disponible){
+            console.log(propiedadesTicket.nombre + ' esta agotado')
+        }
+    }
+}
+console.log(verificarTicketDisponible(
+    [
+        {nombre: 'Bresh', disponible: true},
+        {nombre: 'Kapital', disponible: true},
+        {nombre: 'museum', disponible: false},
+        {nombre: 'LolaPalooza', disponible: false}
+    ]
+))
+
+// Ejercicio 7
+//Han decidido ofrecer un descuento especial del 15% en todas las categorías de tickets.
+// Tenés un array de objetos donde cada objeto representa un ticket, con propiedades
+// nombre (del tipo de ticket) y precio (el precio actual del ticket).
+// Tu tarea es escribir una función que:
+// a. Reciba un array de objetos tickets, donde cada objeto tiene propiedades
+// nombre y precio.
+// b. Utilice el método forEach para recorrer cada ticket en el array.
+// c. Aplique un descuento del 15% al precio de cada ticket.
+// Devuelve el array con los precios actualizados pero necesitamos un backup de los
+// precios viejos
+console.log('Ejercicio 7')
+function aplicarDescuentoTicket(tickets){
+    let descuento = .15
+    let precios = []
+    tickets.forEach(function(ticket){
+        ticket.precio -= ticket.precio * descuento
+        precios.push(ticket.precio)
+    })
+    console.log(precios)
+}
+console.log(aplicarDescuentoTicket(
+    [
+        {nombre: 'bresh', precio: 10000}, 
+        {nombre: 'kapital', precio: 3000},
+        {nombre: 'museum', precio: 5000},
+        {nombre: 'LolaPalooza', precio: 100000}
+    ]
+))
+
+// Ejercicio 8
+// Como encargado de ajustar los precios de los videojuegos en Steam para una nueva
+// región, debes convertir los precios a dólares y realizar ajustes basados en la
+// popularidad de cada juego. Mantén una copia del precio original para referencia.
+// Escribe una función que reciba:
+// Un array de objetos juegos, donde cada objeto tiene propiedades nombre, precio, y
+// popularidad.
+// Una tasa de cambio a dólares.
+// La función debe:
+// a) Convertir los precios a dólares.
+// b) Aplicar un ajuste adicional:
+// i. $5 si la popularidad es 8 o más.
+// ii. $2 si la popularidad es menos de 8.
+// c) Calcular y mostrar el precio promedio de los juegos
+console.log('Ejercicio 8')
+function convertirPreciosAdolares(juegos){
+    let dolar = 1000
+    let precios = []
+    juegos.forEach(function(juego){
+
+        if (juego.popularidad >= 8){
+            juego.precio += 5 * dolar
+        } else if (juego.popularidad < 8){
+            juego.precio += 2 * dolar
+        }
+        console.log(`El precio del ${juego.nombre} es ${juego.precio / dolar}`)
+    })
+}
+console.log(convertirPreciosAdolares(
+    [
+        {nombre: 'FIFA', precio: 60000, popularidad: 8},
+        {nombre: 'GTA', precio: 100000, popularidad: 10},
+        {nombre: 'Among Us', precio: 5000, popularidad: 6},
+        {nombre: 'Fall Guys', precio: 15000, popularidad: 4}
+    ]
+))
+
+// Ejercicio 9
+//   Imaginá que sos el encargado de la tienda de merchandising de Argentina y necesitas
+// actualizar el inventario después de una gran venta celebrando un triunfo. Los hinchas
+// han estado comprando productos como locos, ¡y tu tarea es ajustar el stock!
+// Descripción del Problema:
+// Escribe una función en JavaScript que realice las siguientes tareas:
+// a) Recibe un Array de Productos: Cada objeto en el array representa un producto
+// y tiene las siguientes propiedades:
+// ○ nombre: El nombre del producto (por ejemplo, "Camiseta del Campeón",
+// "Gorra del Dibu").
+// ○ cantidad: La cantidad actual en stock del producto.
+// ○ vendido: La cantidad del producto que se ha vendido.
+// b) Actualizar el Stock:
+// ○ Utiliza el método forEach para recorrer el array de productos.
+// ○ Calcula la cantidad restante de cada producto después de la venta
+// (restando la cantidad vendida de la cantidad en stock).
+// ○ Actualiza la cantidad en stock del producto con el nuevo valor.
+
+// c) Mostrar Mensajes:
+// ○ Crea un mensaje para cada producto que indique si el stock es suficiente
+// o está bajo:
+// ■ Si la cantidad restante es mayor a 10, el mensaje debe ser: "La
+// [nombre del producto] está bien abastecida.
+// Cantidad restante en stock: [cantidad restante]".
+// ■ Si la cantidad restante es 10 o menos, el mensaje debe ser: "¡La
+// [nombre del producto] está en bajo stock! Solo
+// quedan [cantidad restante] unidades.".
+// d) Mostrar los Mensajes en la Consola: Imprime cada mensaje en la consola.
+// e) Calcular el Porcentaje Restante: calcula el porcentaje de stock restante. El
+// porcentaje se calcula como: (cantidadRestante / cantidadOriginal)
+// * 100.
+// Ejemplo de Entrada:
+// const productos = [
+    // { nombre: "Camiseta del Campeón", cantidad: 100, vendido: 30 },
+    // { nombre: "Gorra del Dibu", cantidad: 50, vendido: 20 },
+    // { nombre: "La Bufanda de los Campeones del Mundo", cantidad: 75,
+    // vendido: 25 }];
+    // Salida esperada:
+    // La Camiseta del Campeón está bien abastecida. Cantidad restante en stock: 70.
+    // Porcentaje de stock restante: 70%.
+    // La Gorra del Dibu está bien abastecida. Cantidad restante en stock: 30.
+    // Porcentaje de stock restante: 60%.
+    // ¡La Bufanda de los Campeones del Mundo está en bajo stock! Solo quedan 50
+    // unidades. Porcentaje de stock restante: 66.67%.
+    
+    console.log('Ejercicio 9')
+    function actualizarStock(productos){
+        productos.forEach(function(producto){
+            let cantidadRestante = producto.stock - producto.vendido
+            let porcentajeRestante = (cantidadRestante / producto.stock) * 100
+            if (cantidadRestante > 10){
+                console.log(`El producto ${producto.nombre} esta bien abastecida, cantidad restante en stock: ${cantidadRestante}. Porcentaje de stock restante: ${porcentajeRestante}%`)
+            } else{
+                console.log(`El producto ${producto.nombre} esta en bajo stock! solo quedan: ${cantidadRestante} unidades. Porcentaje de stock restante: ${porcentajeRestante}%`)
+            }
+
+        })
+    }
+    console.log(actualizarStock(
+        [
+            {nombre: 'Camiseta del campeon', stock: 2000, vendido: 20},
+            {nombre: 'Gorra del dibu', stock: 1000, vendido: 100},
+            {nombre: 'Botines de otamendi', stock: 500, vendido: 50},
+            {nombre: 'Media de aguero', stock: 5, vendido: 2}
+        ]
+    ))
+
+// Ejercicio 10
+// Para cada usuario, la función debe:
+// ○ Si el usuario tiene preferencias, recorrer ese array de preferencias y:
+// ■ Verificar si existe una promoción en el objeto promociones para esa
+// preferencia.
+// ■ Si existe una promoción, mostrar un mensaje en la consola que indique
+// que se ha enviado un correo a ese usuario con la promoción aplicada.
+// ■ Si no existe una promoción para la preferencia, mostrar un mensaje en la
+// consola que indique que se ha enviado un correo sin promoción
+// específica.
+
+
+console.log('Ejercicio 10')
+function enviarCorreo (usuarios, promociones){
+    usuarios.forEach(function(usuario){
+        if (usuario.preferencias.length > 0){
+            usuario.preferencias.forEach(function(preferencia){
+                if (promociones[preferencia]){
+                    console.log(`Enviando correo a ${usuario.nombre} (${usuario.email}) sobre ${preferencia} - Promocion: ${promociones[preferencia]}`)
+
+                }
+            })
+        } else{
+            console.log(`Se ha enviado un correo generico a ${usuario.nombre} sin promocion`)
+        }
+    })
+}
+console.log(enviarCorreo(
+    [
+        { nombre: "Juan Pérez", email: "juan.perez@example.com", preferencias:
+        ["Camisetas", "Gorras"] },
+        { nombre: "María García", email: "maria.garcia@example.com", preferencias:
+        ["Bufandas"] },
+        { nombre: "Carlos López", email: "carlos.lopez@example.com", preferencias: []
+    }
+],
+{
+    "Camisetas": "10% de descuento adicional",
+    "Gorras": "15% de descuento adicional",
+    "Bufandas": "20% de descuento adicional"
+}
+))
+// La función debe llevar un conteo de cuántos correos se enviaron en total y
+// cuántas veces se aplicó cada promoción.
+// Al final de la función, mostrar en la consola el total de correos enviados y un resumen de
+// las promociones aplicadas, indicando cuántas veces se aplicó cada una.
+// Salida Esperada:
+// Enviando correo a Juan Pérez (juan.perez@example.com) sobre Camisetas -
+// Promoción: 10% de descuento adicional
+// Enviando correo a Juan Pérez (juan.perez@example.com) sobre Gorras -
+// Promoción: 15% de descuento adicional
+// Enviando correo a María García (maria.garcia@example.com) sobre Bufandas -
+// Promoción: 20% de descuento adicional
+// Enviando correo genérico a Carlos López (carlos.lopez@example.com)
+// Total de correos enviados: 4
+// Promociones aplicadas:
+// - Camisetas: 1
+// - Gorras: 1
+// - Bufandas: 1
+
